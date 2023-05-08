393368f4d58e0391b8286316509091d908add3a2
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2023-05-08 12:09:38 +0530

    meal item page refinment

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/models/meal.dart';
+import 'package:meals_app_flutter/screens/meal_details.dart';
+import 'package:meals_app_flutter/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -17,6 +19,14 @@ class MealItem extends StatelessWidget {
         meal.affordability.name.substring(1);
   }
 
+  void _selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -27,7 +37,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          _selectMeal(context, meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -60,25 +72,25 @@ class MealItem extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 12),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                    //   children: [
-                    //     MealItemTrait(
-                    //       icon: Icons.schedule,
-                    //       label: '${meal.duration} min',
-                    //     ),
-                    //     const SizedBox(width: 12),
-                    //     MealItemTrait(
-                    //       icon: Icons.work,
-                    //       label: complexityText,
-                    //     ),
-                    //     const SizedBox(width: 12),
-                    //     MealItemTrait(
-                    //       icon: Icons.attach_money,
-                    //       label: affordabilityText,
-                    //     )
-                    //   ],
-                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.currency_rupee_sharp,
+                          label: affordabilityText,
+                        )
+                      ],
+                    ),
                   ],
                 ),
               ),
